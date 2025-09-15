36ca665800f53b4c53517376592661b794faea36
team-dataworx <106854273+madalien90@users.noreply.github.com>
2025-09-15 10:12:42 +0530

    Updated db.mjs
    configure according to Render postgresql instance connection..

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -7,11 +7,8 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }          // Required for Render
 });
 
 export default pool;
